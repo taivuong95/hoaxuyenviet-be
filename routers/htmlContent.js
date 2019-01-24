743cf2a50eca1dde140e0582af7c842ad23ebc6f
Thai Nguyen <thai.nguyen@s3corp.com.vn>
2019-01-24 17:43:11 +0700

    add permission to edit HTML CONTENT

--- routers/htmlContent.js
+++ routers/htmlContent.js
@@ -1,20 +1,21 @@
 const router = require("express").Router(),
   util = require("../utils/utilities"),
-  actionName = require("../utils/actionName");
+  actionName = require("../utils/actionName"),
+  jwt = require("../utils/authentication");
 
 let htmlContentModel = require("../models/htmlContentModel");
 
 // Create or update html content
-router.post("/htmlContent", (req, res) => {
+router.post("/htmlContent", jwt.verifyToken, (req, res) => {
   htmlContentModel.findById("htmlContent", (err, data) => {
     if (data) {
       htmlContentModel.updateOne(req.body, (err, data) => {
         util.execFunction(err, actionName.UPDATED, res);
-      })
+      });
     } else {
       htmlContentModel.create(req.body, (err, data) => {
         util.execFunction(err, actionName.CREATED, res);
-      })
+      });
     }
   });
 });
@@ -23,7 +24,7 @@ router.post("/htmlContent", (req, res) => {
 router.get("/htmlContent", (req, res) => {
   htmlContentModel.findOne((err, data) => {
     util.execFunction(err, data, res);
-  })
-})
+  });
+});
 
 module.exports = router;
